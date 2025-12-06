01de98b159d7e8dfabda0f7a283f99ed2ce50e8f
Prem Pillai <prem@squareup.com>
2025-12-06 21:00:30 +1100

    feat: use bun exclusively

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import { Command } from "commander";
 import { detectRuntime } from "../shared/runtime.js";
 import { createLogger, LogLevel, type Logger } from "../shared/logger.js";
